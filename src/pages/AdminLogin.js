76a251124f30d10133db45e5edb9bd75f0d5d142
Arouri <arouri096@gmail.com>
2025-06-06 11:11:02 +0200

    Ajout console log pour vérifier API_BASE_URL

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:4000';
+
+useEffect(() => {
+  console.log("API_BASE_URL =", API_BASE_URL);
+}, []);
 
 
 function AdminLogin() {
